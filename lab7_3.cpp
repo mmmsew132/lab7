8ba0f4f137389792f9025ebc135bd2f951c819e2
mmmsew132 <yayachaidan@gmail.com>
2021-11-30 22:23:45 +0700

    " donen't correct result nasty solution 7_3"

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,6 +2,35 @@
 
 using namespace std;
 
+int adiff(int A, int B){
+  if (A >= 360 || A < -360){
+    A = A%360;
+  }
+
+  if (B >= 360 || B < -360){
+    B = B%360;
+  }
+
+  int C;
+
+  C = A - B;
+
+  if (C < 0){
+    C = C * (0-1);
+  }
+  
+  if (C > 180){
+    if (A > B){
+      A = 360 - A;
+    } else {
+      B = 360 - B;
+    }
+    C = A + B;
+  }
+
+  return C;
+}
+
 
 int main(){
   cout << adiff(180,270);
